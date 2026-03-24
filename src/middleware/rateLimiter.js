c3aad6b59c951dde3e1d048f0c3ade400a224abc
PAUL-23VE <paulvelastegui2016@gmail.com>
2026-03-24 09:17:59 -0500

    Add IP hashing and anonymization middleware for rate limiting

--- src/middleware/rateLimiter.js
+++ src/middleware/rateLimiter.js
@@ -1,8 +1,18 @@
 const rateLimit = require('express-rate-limit');
+const crypto = require('crypto');
+
+function hashIp(ip) {
+  const salt = process.env.IP_HASH_SALT || 'default_salt_change_me';
+  return crypto.createHash('sha256').update(String(ip) + salt).digest('hex');
+}
 
 const denunciaLimiter = rateLimit({
   windowMs: 60 * 60 * 1000, 
   max: 5, 
+  keyGenerator: (req /*, res*/) => {
+    const ip = req.ip || req.headers['x-forwarded-for'] || req.connection?.remoteAddress || '';
+    return hashIp(ip);
+  },
   message: {
     error: 'Has enviado demasiadas denuncias. Intenta de nuevo en 1 hora.',
   },
@@ -10,4 +20,10 @@ const denunciaLimiter = rateLimit({
   legacyHeaders: false,
 });
 
-module.exports = { denunciaLimiter };
+function anonymizeIpMiddleware(req, res, next) {
+  const ip = req.ip || req.headers['x-forwarded-for'] || req.connection?.remoteAddress || '';
+  req.anonymizedIp = hashIp(ip);
+  next();
+}
+
+module.exports = { denunciaLimiter, hashIp, anonymizeIpMiddleware };
